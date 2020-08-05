86b7eaba8a03e5c4b8aa1536ca61de71df0dc835
sam yoeun <sam.yoeun@pathmazing.com>
2020-08-05 21:43:04 +0700

    Initial Commit

---
+++ ios-lottie-animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-lottie-animation
+//
+//  Created by Samrith Yoeun on 8/5/20.
+//  Copyright © 2020 Sammi Yoeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
