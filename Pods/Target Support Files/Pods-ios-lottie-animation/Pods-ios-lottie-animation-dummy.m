110a25e04d1bccef8e614c60861d490a57abab33
sam yoeun <sam.yoeun@pathmazing.com>
2020-08-05 23:37:41 +0700

    update

---
+++ Pods/Target Support Files/Pods-ios-lottie-animation/Pods-ios-lottie-animation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_lottie_animation : NSObject
+@end
+@implementation PodsDummy_Pods_ios_lottie_animation
+@end
--- ios-lottie-animation/ViewController.swift
+++ ios-lottie-animation/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        var myView = LOTAnimationView(name: "touchId")
+        myView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        myView.contentMode = .scaleAspectFit
+        myView.autoReverseAnimation = true
+        myView.loopAnimation = true
+        myView.play()
+        
+        self.view.addSubview(myView)
+        
+        var faceIdView = LOTAnimationView(name: "faceId")
+        faceIdView.frame = CGRect(x: 100, y: 400, width: 100, height: 100)
+        faceIdView.contentMode = .scaleAspectFit
+        
+        faceIdView.animationSpeed = 1.2
+        faceIdView.autoReverseAnimation = true
+        faceIdView.loopAnimation = true
+        faceIdView.play(fromFrame: 0, toFrame: 75) { _ in
+            
+        }
+        
+        self.view.addSubview(faceIdView)
+//
+//        var faceIdView = AnimationView()
+//        faceIdView.frame = CGRect(x: 100, y: 400, width: 200, height: 200)
+//        faceIdView.animation = Animation.named("faceId")
+//
+//        faceIdView.animationSpeed = 1
+//        //        myView.backgroundColor = .red
+//        faceIdView.play(fromFrame: .init(0), toFrame: 75, loopMode: .playOnce) { completed in
+//            print("--- completed")
+//        }
+//        self.view.addSubview(faceIdView)
     }
-
-
+    
+    
 }
 
